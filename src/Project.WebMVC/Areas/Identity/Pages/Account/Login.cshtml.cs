da2d71244bf27c9723957d6115b4c4c15e7f3bab
Rostislav <statko.rostik@gmail.com>
2021-05-18 17:14:50 +0300

    Add login and register

---
+++ src/Project.WebMVC/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Project.WebMVC.Areas.Identity.Pages.Account
+{
+    public class LoginModel : PageModel
+    {
+        private readonly SignInManager<IdentityUser> _signInManager;
+
+        public LoginModel(SignInManager<IdentityUser> signInManager)
+        {
+            _signInManager = signInManager;
+        }
+
+        [BindProperty]
+        public InputModel Input { get; set; }
+
+        public class InputModel
+        {
+            [Required]
+            public string Username { get; set; }
+
+            [Required]
+            [DataType(DataType.Password)]
+            public string Password { get; set; }
+        }
+
+        public void OnGet()
+        {
+
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            var signInReuslt = await _signInManager.PasswordSignInAsync(Input.Username, Input.Password, false, false);
+
+            if (signInReuslt.Succeeded)
+            {
+                return RedirectToAction("Index");
+            }
+
+            return Page();
+        }
+    }
+}
