c51af2c502d64243afe863ea8a660edeb75d6ad3
Rostislav <statko.rostik@gmail.com>
2021-05-19 23:05:01 +0300

    Refactor

--- src/Project.WebMVC/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ src/Project.WebMVC/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -3,18 +3,25 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Project.WebMVC.Identity;
 
 namespace Project.WebMVC.Areas.Identity.Pages.Account
 {
+    [AllowAnonymous]
     public class LoginModel : PageModel
     {
-        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
 
-        public LoginModel(SignInManager<IdentityUser> signInManager)
+        public LoginModel(
+            UserManager<AppUser> userManager,
+            SignInManager<AppUser> signInManager)
         {
+            _userManager = userManager;
             _signInManager = signInManager;
         }
 
@@ -42,14 +49,16 @@ namespace Project.WebMVC.Areas.Identity.Pages.Account
             {
                 return Page();
             }
+            
+            var signInResult = await _signInManager.PasswordSignInAsync(Input.Username, Input.Password, false, false);
 
-            var signInReuslt = await _signInManager.PasswordSignInAsync(Input.Username, Input.Password, false, false);
-
-            if (signInReuslt.Succeeded)
+            if (signInResult.Succeeded)
             {
                 return RedirectToAction("Index");
             }
 
+            ModelState.AddModelError("Input.Username", "Fail to login");
+            
             return Page();
         }
     }
