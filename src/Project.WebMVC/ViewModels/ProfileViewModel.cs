7c005251c3247e45c040431be072af985e9798f6
Rostislav <statko.rostik@gmail.com>
2021-05-21 18:05:42 +0300

    Add user profile

---
+++ src/Project.WebMVC/ViewModels/ProfileViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Project.WebMVC.ViewModels
+{
+    public class ProfileViewModel
+    {
+        public string Username { get; set; }
+        public string ProfileImageUrl { get; set; }
+    }
+}
