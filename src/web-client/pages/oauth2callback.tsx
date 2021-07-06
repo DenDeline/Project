2d2658d5cc65a77e3d397a1b8c29a13fc46f0152
Rostislav <statko.rostik@gmail.com>
2021-07-06 14:42:07 +0300

    Implement link generation to authorize endpoint

---
+++ src/web-client/pages/oauth2callback.tsx
@@ -0,0 +1,18 @@
+﻿import {GetServerSideProps} from "next";
+
+
+
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+    return {
+        redirect: {
+            statusCode: 302,
+            destination: "http://localhost:3000"
+        }
+    }
+}
+
+export default function Oauth2callback(){
+    return <></>;
+}
+
