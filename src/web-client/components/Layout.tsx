765d25f5516f5833dbfe5618ff8cdb8f7fe9e2f2
Rostislav <statko.rostik@gmail.com>
2021-07-06 14:36:36 +0300

    Add material-ui, js-sha256, randexp libs

---
+++ src/web-client/components/Layout.tsx
@@ -0,0 +1,25 @@
+﻿import React from "react";
+import Link from "next/link";
+
+const Layout: React.FC = ({children}) => {
+    return(
+        <>
+            <div>
+                <ul>
+                    <li>
+                        <Link href={'/'}>Home</Link>
+                    </li>
+                </ul>
+            </div>
+            
+            <div>
+                {
+                    children
+                }
+            </div>
+            
+        </>
+    );
+}
+
+export default Layout;
