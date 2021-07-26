2a546255969194bccd42c0fb7546bf36678a4b98
Rostislav <statko.rostik@gmail.com>
2021-07-27 00:05:43 +0300

    feature: add role panel page

---
+++ src/web-client/components/ConfigureUserDialog.tsx
@@ -0,0 +1,196 @@
+﻿import React, {useCallback, useEffect, useState} from "react";
+import {
+    Button, Checkbox, Chip, CircularProgress, createStyles,
+    Dialog,
+    DialogActions,
+    DialogContent,
+    DialogContentText,
+    DialogTitle, FormControl, FormControlLabel, FormGroup, FormLabel, Grid, Input, makeStyles, Menu, MenuItem, Paper,
+    TextField, Theme
+} from "@material-ui/core";
+import Link from 'next/link'
+import {Add} from "@material-ui/icons";
+
+
+interface ConfigureUserDialogProps {
+    open: boolean,
+    user?: {
+        id: number,
+        username: string,
+        fullname: string,
+        verified: boolean
+        roles: string[]
+    },
+    availableRoles: string[]
+    onSave: (user: any) => void
+    onClose: () => void
+}
+
+const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+        root: {
+            display: 'flex',
+            flexWrap: 'wrap',
+            listStyle: 'none',
+            padding: theme.spacing(0.5),
+            margin: 0,
+        },
+        chip: {
+            margin: theme.spacing(0.5),
+        },
+    }),
+);
+
+const ConfigureUserDialog: React.FC<ConfigureUserDialogProps> = (props) => {
+    const classes = useStyles();
+    
+    const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+
+    const handleMenuClose = useCallback(() => {
+        setAnchorEl(null);
+    }, [setAnchorEl]);
+
+    const handleMenuClick = useCallback((event: React.MouseEvent<HTMLDivElement>) => {
+        setAnchorEl(event.currentTarget);
+    }, []);
+    
+    const [isUserChanged, setIsUserChanged] = useState<boolean>(false);
+    
+    const [userRoles, setUserRoles] = useState<string[]>( []);
+    
+    const handleAppendRole = useCallback((role: string) => {
+        setUserRoles(userRoles.concat(role));
+        setIsUserChanged(true);
+    }, [userRoles])
+    
+    const handleDeleteRole = useCallback((role: string) => {
+        setUserRoles(userRoles.filter(_ => _ !== role));
+        setIsUserChanged(true);
+    }, [userRoles]);
+    
+    const [userVerified, setUserVerified] = useState<boolean>(false);
+    
+    const handleUserVerification = useCallback((verified: boolean) => {
+        setUserVerified(verified);
+        if (!verified){
+            setUserRoles([]);
+        }
+        setIsUserChanged(true);
+    }, []);
+    
+    const [availableRolesForUser, setAvailableRolesForUser] = useState<string[]>([]);
+
+    useEffect(() => {
+        setUserRoles(props.user?.roles.slice() ?? []);
+        setUserVerified(props.user?.verified ?? false);
+    }, [props.user]);
+    
+    useEffect(() => {
+        setAvailableRolesForUser(props.availableRoles.filter(_ => ! userRoles.includes(_)));
+    }, [props.availableRoles, userRoles]);
+    
+    return (
+        <Dialog fullWidth={true} maxWidth={'sm'} open={props.open} aria-labelledby="dialog-title" onClose={props.onClose}>
+            <DialogTitle id={"dialog-title"}>Configure user: {props.user?.username}</DialogTitle>
+            <DialogContent>
+                <DialogContentText>
+                    Manage user account
+                </DialogContentText>
+                <Grid container direction={'column'} spacing={2}>
+                    <Grid item>
+                        <TextField label={"Username"} value={props.user?.username} disabled/>
+                    </Grid>
+                    <Grid item>
+                        <TextField label={"Full name"} value={props.user?.fullname} disabled/>
+                    </Grid>
+                    <Grid item>
+                        <DialogContentText>
+                            Only approved users have access for our resource <br/>
+                            Links: <Link href={'/'}>Images</Link>
+                        </DialogContentText>
+                        <FormControlLabel control={<Checkbox checked={userVerified}/>} onClick={() => {
+                            handleUserVerification(!userVerified);
+                        }} label={"Documents approved"}/>
+                    </Grid>
+                    {
+                        userVerified && (
+                            <Grid item>
+                                <DialogContentText>
+                                    User roles:
+                                </DialogContentText>
+                                <Paper component={'ul'} className={classes.root} variant={'outlined'}>
+                                    {
+                                        userRoles.map((userRole, index) => (
+                                            <li key={index}>
+                                                <Chip
+                                                    label={userRole}
+                                                    color={'primary'}
+                                                    onDelete={() => {
+                                                        handleDeleteRole(userRole);
+                                                    }}
+                                                    className={classes.chip}/>
+                                            </li>
+                                        ))
+                                    }
+                                    <li key={userRoles.length}>
+                                        <Chip
+                                            aria-controls="user-roles-menu"
+                                            icon={<Add fontSize={'small'}/>}
+                                            label={"Add role"}
+                                            color={'default'}
+                                            onClick={handleMenuClick}
+                                            className={classes.chip}/>
+                                        <Menu
+                                            id="user-roles-menu"
+                                            anchorEl={anchorEl}
+                                            keepMounted
+                                            open={Boolean(anchorEl)}
+                                            onClose={handleMenuClose}
+                                        >
+                                            {
+                                                availableRolesForUser.length > 0 ?
+                                                    availableRolesForUser.map(role => (
+                                                        <MenuItem onClick={() => {
+                                                            handleAppendRole(role);
+                                                            handleMenuClose();}}
+                                                        >
+                                                            {
+                                                                role 
+                                                            }
+                                                        </MenuItem>)) : 
+                                                    (
+                                                        <MenuItem onClick={handleMenuClose}>None</MenuItem>
+                                                    )
+                                            }
+                                        </Menu>
+                                    </li>
+                                </Paper>
+                            </Grid>
+                        )
+                    }
+                </Grid>
+            </DialogContent>
+            <DialogActions>
+                <Button onClick={props.onClose} color={'primary'}>
+                    Close
+                </Button>
+                <Button onClick={() => {
+                    if (isUserChanged){
+                        props.onSave({
+                            id: props.user?.id,
+                            username: props.user?.username,
+                            fullname: props.user?.fullname,
+                            verified: userVerified,
+                            roles: userRoles
+                        });
+                    }
+                    props.onClose();
+                }} color={'primary'} variant={'contained'}>
+                    Save
+                </Button>
+            </DialogActions>
+        </Dialog>
+    );
+}
+
+export default ConfigureUserDialog;
--- src/web-client/pages/role-panel/index.tsx
+++ src/web-client/pages/role-panel/index.tsx
@@ -1,11 +1,23 @@
 ﻿import {GetServerSideProps} from "next";
-import React, {useState} from "react";
-import {DataGrid, GridColDef} from '@material-ui/data-grid';
+import React, {useCallback, useState} from "react";
+import {DataGrid, GridCellParams, GridColDef} from '@material-ui/data-grid';
 import Link from 'next/link'
-import {Card, Container, createStyles, Grid, Link as MaterialLink, makeStyles, Paper, Typography} from "@material-ui/core";
+import {
+    Button,
+    Card,
+    Chip,
+    Container,
+    createStyles,
+    Grid,
+    Link as MaterialLink,
+    makeStyles,
+    Paper, Select,
+    Typography
+} from "@material-ui/core";
 import Navbar from "../../components/Navbar";
 import CheckIcon from '@material-ui/icons/Check';
 import ClearIcon from '@material-ui/icons/Clear';
+import ConfigureUserDialog from "../../components/ConfigureUserDialog";
 
 const useStyles = makeStyles(theme => 
     createStyles({
@@ -21,18 +33,60 @@ const useStyles = makeStyles(theme =>
 );
 
 const RolePanel: React.FC = (props) => {
+    
     const classes = useStyles();
+    const [users, setUsers] = useState(
+        [
+            {id : 1, username: 'admin', fullname: 'Rostislav Statko', verified: true, roles: ['Administrator']},
+            {id : 2, username: 'DenDeline', fullname: 'Rostislav Statko', verified: false, roles: []},
+        ]
+    );
+    
+    const [availableRoles, setAvailableRoles] = useState([
+        'Representative Authority', 'Authority', 'Administrator'
+    ]);
+    
+    const [selectedUser, setSelectedUser] = useState(undefined);
+    
+    const [open, setOpen] = useState(false);
+    
+    const handleClickOpen = (user: any) => {
+        setSelectedUser(user);
+        setOpen(true);
+    };
+    
+    const handleClose = () => {
+        setOpen(false);
+    };
+    
+    const handleSaveUser = useCallback((user: any) => {
+        setUsers(users.filter(_ => _.id !== user.id).concat(user));
+    }, [users]);
     
     const columns: GridColDef[] = [
-        { field: 'id', headerName: 'ID' },
+        { field: 'id', headerName: 'ID', disableReorder: true },
         { field: 'username', headerName: 'Username', flex: 1 },
         { field: 'fullname', headerName: 'Full Name', flex: 1 },
-        { field: 'hasApprovedDocs', flex: 1, headerName: 'Documents approved', renderCell: params => (params.value ? <CheckIcon color={'primary'}/> : <ClearIcon color={'primary'}/>)}
-    ];
-    
-    const rows = [
-        {id : 1, username: 'admin', fullname: 'Rostislav Statko', hasApprovedDocs: true},
-        {id : 2, username: 'DenDeline', fullname: 'Rostislav Statko', hasApprovedDocs: false},
+        { field: 'roles', headerName: 'Roles', flex: 1, renderCell: (params: GridCellParams) => ( 
+            <Grid container spacing={1}>
+                {
+                    (params.value as string[]).map((item, index) => (
+                        <Grid item key={index}>
+                            <Chip label={item} color={'primary'}/>
+                        </Grid>
+                    ))
+                    
+                }
+            </Grid>
+            ) },
+        { field: 'verified', flex: 1, headerName: 'Documents approved', renderCell: params => (params.value ? <CheckIcon color={'primary'}/> : <ClearIcon color={'primary'}/>)},
+        { field: 'actions', flex: 1, headerName: 'Actions', renderCell: (params: GridCellParams) => (
+            <Grid container>
+                <Grid item>
+                    <Button onClick={() => handleClickOpen(params.row)}>Edit</Button>
+                </Grid>
+            </Grid>
+            )}
     ];
     
     return (
@@ -48,32 +102,24 @@ const RolePanel: React.FC = (props) => {
                                 </Typography>
                             </Grid>
                             <Grid item container direction={'column'} className={classes.content}>
-                                <Grid item>
-                                    
-                                </Grid>
-                                <Grid item container >
-                                    <Grid item sm={6} container direction={'column'}>
-                                        <Grid item>
-                                            <Typography variant={'h5'} gutterBottom>
-                                                1) Select users
-                                            </Typography>
-                                        </Grid>
-                                        <Grid item>
-                                            <DataGrid
-                                                checkboxSelection
-                                                autoHeight
-                                                rows={rows}
-                                                columns={columns}
-                                                pagination
-                                                pageSize={30}
-                                                rowCount={10}
-                                            />
-                                        </Grid>
-                                    </Grid>
-                                </Grid>
+                                <DataGrid
+                                    autoHeight
+                                    rows={users}
+                                    columns={columns}
+                                    pagination
+                                    pageSize={30}
+                                    rowCount={10}
+                                />
                             </Grid>
                         </Grid>
                     </Paper>
+                    <ConfigureUserDialog 
+                        open={open} 
+                        user={selectedUser}
+                        availableRoles={availableRoles}
+                        onSave={handleSaveUser}
+                        onClose={handleClose}
+                    />
                 </Typography>
             </Container>
         </>
