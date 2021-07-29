946db79f97cf1a4aceba4ce627af038f8f150f22
Rostislav <statko.rostik@gmail.com>
2021-07-29 14:01:25 +0300

    refactor (VoteBackend): remove unused pages

--- Project.VoteBackend/src/Project.WebMVC/Controllers/HomeController.cs
+++ Project.VoteBackend/src/Project.WebMVC/Controllers/HomeController.cs
@@ -1,25 +1,33 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
 using Project.WebMVC.ViewModels;
 
 namespace Project.WebMVC.Controllers
 {
-    public class HomeController : Controller
+  public class HomeController : Controller
+  {
+    private readonly IWebHostEnvironment _environment;
+
+    public HomeController(IWebHostEnvironment environment)
     {
-        public IActionResult Index()
-        {
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+      _environment = environment;
     }
+    public IActionResult Index() 
+    {
+      if (_environment.IsProduction())
+      {
+        return NotFound();
+      }
+      return Ok("VoteBackend project"); 
+    }
+
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error() 
+    { 
+      return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+  }
 }
