31d023fd83fd1ab05663028fa65b458a71e49b47
Rostislav <statko.rostik@gmail.com>
2021-07-29 14:25:50 +0300

    feature (VoteBackend): add swagger

--- Project.VoteBackend/src/Project.WebMVC/Controllers/OauthController.cs
+++ Project.VoteBackend/src/Project.WebMVC/Controllers/OauthController.cs
@@ -13,168 +13,170 @@ using Project.WebMVC.ViewModels;
 
 namespace Project.WebMVC.Controllers
 {
-    public class OauthController: Controller
+  public class OauthController: Controller
+  {
+    private readonly UserManager<AppUser> _userManager;
+
+    public OauthController(UserManager<AppUser> userManager)
     {
-        private readonly UserManager<AppUser> _userManager;
-
-        public OauthController(UserManager<AppUser> userManager)
-        {
-            _userManager = userManager;
-        }
-        
-        [HttpGet("/oauth2/authorize")]
-        public IActionResult Authorize(
-            [FromQuery] string response_type,
-            [FromQuery] string client_id,
-            [FromQuery] string redirect_uri,
-            [FromQuery] string state,
-            [FromQuery] string code_challenge,
-            [FromQuery] string code_challenge_method)
-        {
-            var client = AuthServerConfig.InMemoryClients.FirstOrDefault(_ => _.ClientId == client_id);
-            
-            // TODO: Separate class for validation required params 
-            if (client is null)
-            {
-                return BadRequest();
-            }
-
-            if (!client.RedirectUris.Contains(redirect_uri))
-            {
-                //TODO: Add error validation constant: invalid_request
-                var queryBuilder = new QueryBuilder
-                {
-                    {"error", "invalid_request"},
-                    {"state", state}
-                };
-                return Redirect($"{redirect_uri}{queryBuilder}");
-            }
-
-            if (AuthServerConfig.SupportedResponseTypes.All(_ => _ != response_type))
-            {
-                //TODO: Add error validation constant: unsupported_response_type
-                var queryBuilder = new QueryBuilder
-                {
-                    {"error", "unsupported_response_type"},
-                    {"state", state}
-                };
-                return Redirect($"{redirect_uri}{queryBuilder}");
-            }
-
-            var vm = new AuthorizeViewModel
-            {
-                ClientId = client_id,
-                RedirectUri = redirect_uri,
-                State = state,
-                CodeChallenge = code_challenge,
-                CodeChallengeMethod = code_challenge_method
-            };
-            
-            return View(vm);
-        }
-
-        [HttpPost("/oauth2/signin-code")]
-        public async Task<IActionResult> SignInCode(AuthorizeViewModel vm)
-        {
-            var user = (await _userManager.FindByEmailAsync(vm.Login)) ?? (await  _userManager.FindByNameAsync(vm.Login));
-
-            if (user is null)
-            {
-                // TODO: Add validation error and append query params
-                ModelState.AddModelError(vm.Login, "");
-                return View("Authorize", vm);
-            }
-
-            var isPasswordValid =  await _userManager.CheckPasswordAsync(user, vm.Password);
-
-            if (!isPasswordValid)
-            {
-                // TODO: Add validation error and append query params
-                ModelState.AddModelError(vm.Password, "");
-                return View("Authorize", vm);
-            }
-            
-            
-            // TODO: Move out code token generation
-            var client = AuthServerConfig.InMemoryClients.FirstOrDefault(_ => _.ClientId == vm.ClientId);
-            
-            if (client is null)
-            {
-                // TODO: Add validation error and append query params
-                ModelState.AddModelError("", "");
-                return View("Authorize", vm);
-            }
-
-            var codeToken = new CodeToken
-            {
-                ClientId = vm.ClientId,
-                RedirectUri = vm.RedirectUri,
-                UserId = user.Id,
-                CodeChallenge = vm.CodeChallenge,
-                CodeChallengeMethod = vm.CodeChallengeMethod
-            };
-
-            var jsonCodeToken = JsonConvert.SerializeObject(codeToken);
-
-            var encodedCodeToken = jsonCodeToken.AesEncrypt(client.ClientSecret);
-
-            // TODO: Create separate class for building redirectUrl
-            var queryBuilder = new QueryBuilder
-            {
-                {"code", encodedCodeToken}, 
-                {"state", vm.State}
-            };
-
-            var redirectUrl = $"{client.RedirectUris.FirstOrDefault()}{queryBuilder}";
-            return Redirect(redirectUrl);
-        }
-
-
-        [HttpPost("/oauth2/token")]
-        public async Task<IActionResult> GetAccessTokenAsync(
-            [FromBody] TokenRequest vm,
-            [FromServices] ITokenService tokenService,
-            CancellationToken cts = new CancellationToken())
-        {
-            if (AuthServerConfig.SupportedGrantTypes.All(_ => _ != vm.grant_type))
-            {
-                // TODO: Add validation error
-                return BadRequest();
-            };
-            
-            var client = AuthServerConfig.InMemoryClients.FirstOrDefault(_ => _.ClientId == vm.client_id);
-            if (client is null)
-            {
-                // TODO: Add validation error
-                return BadRequest();
-            }
-
-            var decodedCodeToken = vm.code.AesDecrypt(client.ClientSecret);
-            var codeToken = JsonConvert.DeserializeObject<CodeToken>(decodedCodeToken);
-
-            if (!codeToken.IsValid(vm.client_id, vm.redirect_uri, "S256", vm.code_verifier))
-            {
-                return BadRequest();
-            };
-            
-            var certificate = new SigningIssuerCertificate();
-            
-            var encodedJwtResult = await tokenService.CreateAccessTokenAsync(
-                codeToken.UserId, 
-                certificate.GetPrivateKey(),
-                cts);
-
-            if (!encodedJwtResult.IsSuccess)
-            {
-                return BadRequest();
-            }
-            
-            return Ok(new
-            {
-                access_token = encodedJwtResult.Value,
-                token_type = "bearer",
-                expires_in = 3600
-            });
-        }
+      _userManager = userManager;
     }
+    
+    [HttpGet("/oauth2/authorize")]
+    [ApiExplorerSettings(IgnoreApi = true)]
+    public IActionResult Authorize(
+      [FromQuery] string response_type,
+      [FromQuery] string client_id,
+      [FromQuery] string redirect_uri,
+      [FromQuery] string state,
+      [FromQuery] string code_challenge,
+      [FromQuery] string code_challenge_method)
+    {
+      var client = AuthServerConfig.InMemoryClients.FirstOrDefault(_ => _.ClientId == client_id);
+
+      // TODO: Separate class for validation required params 
+      if (client is null)
+      {
+       return BadRequest();
+      }
+
+      if (!client.RedirectUris.Contains(redirect_uri))
+      {
+        //TODO: Add error validation constant: invalid_request
+        var queryBuilder = new QueryBuilder
+        {
+          {"error", "invalid_request"},
+          {"state", state}
+        };
+        return Redirect($"{redirect_uri}{queryBuilder}");
+      }
+
+      if (AuthServerConfig.SupportedResponseTypes.All(_ => _ != response_type))
+      {
+        //TODO: Add error validation constant: unsupported_response_type
+        var queryBuilder = new QueryBuilder
+        {
+        {"error", "unsupported_response_type"},
+        {"state", state}
+        };
+        return Redirect($"{redirect_uri}{queryBuilder}");
+      }
+
+      var vm = new AuthorizeViewModel
+      {
+        ClientId = client_id,
+        RedirectUri = redirect_uri,
+        State = state,
+        CodeChallenge = code_challenge,
+        CodeChallengeMethod = code_challenge_method
+      };
+
+      return View(vm);
+    }
+
+    [HttpPost("/oauth2/signin-code")]
+    [ApiExplorerSettings(IgnoreApi = true)]
+    public async Task<IActionResult> SignInCode(AuthorizeViewModel vm)
+    {
+      var user = (await _userManager.FindByEmailAsync(vm.Login)) ?? (await  _userManager.FindByNameAsync(vm.Login));
+
+      if (user is null)
+      {
+        // TODO: Add validation error and append query params
+        ModelState.AddModelError(vm.Login, "");
+        return View("Authorize", vm);
+      }
+
+      var isPasswordValid =  await _userManager.CheckPasswordAsync(user, vm.Password);
+
+      if (!isPasswordValid)
+      {
+        // TODO: Add validation error and append query params
+        ModelState.AddModelError(vm.Password, "");
+        return View("Authorize", vm);
+      }
+      
+      // TODO: Move out code token generation
+      var client = AuthServerConfig.InMemoryClients.FirstOrDefault(_ => _.ClientId == vm.ClientId);
+
+      if (client is null)
+      {
+        // TODO: Add validation error and append query params
+        ModelState.AddModelError("", "");
+        return View("Authorize", vm);
+      }
+
+      var codeToken = new CodeToken
+      {
+        ClientId = vm.ClientId,
+        RedirectUri = vm.RedirectUri,
+        UserId = user.Id,
+        CodeChallenge = vm.CodeChallenge,
+        CodeChallengeMethod = vm.CodeChallengeMethod
+      };
+
+      var jsonCodeToken = JsonConvert.SerializeObject(codeToken);
+
+      var encodedCodeToken = jsonCodeToken.AesEncrypt(client.ClientSecret);
+
+      // TODO: Create separate class for building redirectUrl
+      var queryBuilder = new QueryBuilder
+      {
+        {"code", encodedCodeToken}, 
+        {"state", vm.State}
+      };
+
+      var redirectUrl = $"{client.RedirectUris.FirstOrDefault()}{queryBuilder}";
+      return Redirect(redirectUrl);
+    }
+
+
+    [HttpPost("/oauth2/token")]
+    [ApiExplorerSettings(IgnoreApi = true)]
+    public async Task<IActionResult> GetAccessTokenAsync(
+      [FromBody] TokenRequest vm,
+      [FromServices] ITokenService tokenService,
+      CancellationToken cts = new CancellationToken())
+    {
+      if (AuthServerConfig.SupportedGrantTypes.All(_ => _ != vm.grant_type))
+      {
+        // TODO: Add validation error
+        return BadRequest();
+      };
+
+      var client = AuthServerConfig.InMemoryClients.FirstOrDefault(_ => _.ClientId == vm.client_id);
+      if (client is null)
+      {
+        // TODO: Add validation error
+        return BadRequest();
+      }
+
+      var decodedCodeToken = vm.code.AesDecrypt(client.ClientSecret);
+      var codeToken = JsonConvert.DeserializeObject<CodeToken>(decodedCodeToken);
+
+      if (!codeToken.IsValid(vm.client_id, vm.redirect_uri, "S256", vm.code_verifier))
+      {
+        return BadRequest();
+      };
+
+      var certificate = new SigningIssuerCertificate();
+
+      var encodedJwtResult = await tokenService.CreateAccessTokenAsync(
+      codeToken.UserId, 
+      certificate.GetPrivateKey(),
+      cts);
+
+      if (!encodedJwtResult.IsSuccess)
+      {
+        return BadRequest();
+      }
+
+      return Ok(new
+      {
+        access_token = encodedJwtResult.Value,
+        token_type = "bearer",
+        expires_in = 3600
+      });
+    }
+  }
 }
--- Project.VoteBackend/src/Project.WebMVC/Startup.cs
+++ Project.VoteBackend/src/Project.WebMVC/Startup.cs
@@ -7,122 +7,130 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using Project.ApplicationCore;
 using Project.ApplicationCore.Entities;
 using Project.ApplicationCore.Interfaces;
 using Project.Infrastructure.Data;
-using Project.WebMVC.MappingProfiles;
 
 namespace Project.WebMVC
 {
-    public class Startup
+  public class Startup
+  {
+    public Startup(IConfiguration configuration, IWebHostEnvironment environment)
     {
-        public Startup(IConfiguration configuration, IWebHostEnvironment environment)
-        {
-            Configuration = configuration;
-            Environment = environment;
-        }
-
-        public string NetJSClientCorsPolicy = "NetJSClientCorsPolicy";
-        
-        public IConfiguration Configuration { get; }
-        public IWebHostEnvironment Environment { get; }
-
-        public void ConfigureServices(IServiceCollection services)
-        {
-
-            services.AddApplicationCore();
-
-            services.AddAutoMapper(typeof(Startup).Assembly);
-
-            services.AddDbContext<AppDbContext>(options =>
-            {
-                if (Environment.IsDevelopment())
-                {
-                    options.EnableSensitiveDataLogging();
-                }
-                
-                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
-            });
-
-            services.AddScoped<IDbContext, AppDbContext>();
-
-            services.AddIdentity<AppUser, IdentityRole<int>>(config =>
-                {
-                    config.Password.RequireDigit = false;
-                    config.Password.RequiredLength = 4;
-                    config.Password.RequireLowercase = false;
-                    config.Password.RequireUppercase = false;
-                    config.Password.RequiredUniqueChars = 1;
-                    config.Password.RequireNonAlphanumeric = false;
-                })
-                .AddDefaultUI()
-                .AddEntityFrameworkStores<AppDbContext>()
-                .AddDefaultTokenProviders();
-
-            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-                .AddGoogle(config =>
-                {
-                    IConfigurationSection googleConfig =
-                        Configuration.GetSection("Authentication:Google");
-
-                    config.ClientId = googleConfig["ClientId"];
-                    config.ClientSecret = googleConfig["ClientSecret"];
-                })
-                
-                .AddJwtBearer(options =>
-                {
-                    var secretKey = new SigningIssuerCertificate().GetPublicKey();
-                    
-                    options.TokenValidationParameters = new TokenValidationParameters
-                    {
-                        ValidIssuer = "https://localhost:44307",
-                        ValidAudience = "https://localhost:44307",
-                        IssuerSigningKey = secretKey,
-                    };
-                });
-                
-            services.AddCors(options =>
-            {
-                options.AddPolicy(NetJSClientCorsPolicy, builder =>
-                {
-                    builder.AllowAnyHeader();
-                    builder.WithOrigins("http://localhost:3000");
-                    builder.AllowAnyMethod();
-                });
-            });
-            
-            services.AddControllersWithViews();
-            services.AddRazorPages();
-        }
-
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseDatabaseErrorPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-                app.UseHsts();  
-            }
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
-
-            app.UseCors(NetJSClientCorsPolicy);
-            
-            app.UseRouting();
-
-            app.UseAuthentication();
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapDefaultControllerRoute();
-                endpoints.MapRazorPages();
-            });
-        }
+      Configuration = configuration;
+      Environment = environment;
     }
+
+    public const string NetJsClientCorsPolicy = "NetJSClientCorsPolicy";
+
+    public IConfiguration Configuration { get; }
+    public IWebHostEnvironment Environment { get; }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+      services.AddApplicationCore();
+
+      services.AddAutoMapper(typeof(Startup).Assembly);
+
+      services.AddDbContext<AppDbContext>(options =>
+      {
+        if (Environment.IsDevelopment())
+        {
+          options.EnableSensitiveDataLogging();
+        }
+        options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+      });
+
+      services.AddScoped<IDbContext, AppDbContext>();
+
+      services.AddIdentity<AppUser, IdentityRole<int>>(config =>
+      {
+        config.Password.RequireDigit = false;
+        config.Password.RequiredLength = 4;
+        config.Password.RequireLowercase = false;
+        config.Password.RequireUppercase = false;
+        config.Password.RequiredUniqueChars = 1;
+        config.Password.RequireNonAlphanumeric = false;
+      })
+        .AddDefaultUI()
+        .AddEntityFrameworkStores<AppDbContext>()
+        .AddDefaultTokenProviders();
+
+      services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+        .AddGoogle(config =>
+        {
+          IConfigurationSection googleConfig = Configuration.GetSection("Authentication:Google");
+
+          config.ClientId = googleConfig["ClientId"];
+          config.ClientSecret = googleConfig["ClientSecret"];
+        })
+
+      .AddJwtBearer(options =>
+      {
+        var secretKey = new SigningIssuerCertificate().GetPublicKey();
+
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+          ValidIssuer = "https://localhost:44307",
+          ValidAudience = "https://localhost:44307",
+          IssuerSigningKey = secretKey,
+        };
+      });
+
+      services.AddCors(options =>
+      {
+        options.AddPolicy(NetJsClientCorsPolicy, builder =>
+        {
+          builder.AllowAnyHeader();
+          builder.WithOrigins("http://localhost:3000");
+          builder.AllowAnyMethod();
+        });
+      });
+
+      services.AddControllersWithViews();
+      services.AddRazorPages();
+
+      services.AddSwaggerGen(c =>
+      {
+        c.SwaggerDoc("v1", new OpenApiInfo { Title = "Vote WebAPI", Version = "v1" });
+      });
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+      if (env.IsDevelopment())
+      {
+        app.UseDeveloperExceptionPage();
+        app.UseDatabaseErrorPage();
+      }
+      else
+      {
+        app.UseExceptionHandler("/Home/Error");
+        app.UseHsts();  
+      }
+      app.UseHttpsRedirection();
+      app.UseStaticFiles();
+
+      app.UseCors(NetJsClientCorsPolicy);
+
+      app.UseSwagger();
+      app.UseSwaggerUI(c =>
+      {
+        c.SwaggerEndpoint("v1/swagger.json", "My API V1");
+      });
+      
+      app.UseRouting();
+
+      app.UseAuthentication();
+      app.UseAuthorization();
+
+      app.UseEndpoints(endpoints =>
+      {
+        endpoints.MapDefaultControllerRoute();
+        endpoints.MapRazorPages();
+      });
+    }
+  }
 }
