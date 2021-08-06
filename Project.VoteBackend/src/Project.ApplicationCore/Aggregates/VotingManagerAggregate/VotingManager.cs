64ef13fae3a545e41bdfcffdea9c3604d80ccc68
Rostislav <statko.rostik@gmail.com>
2021-08-06 11:46:29 +0300

    refactor (VoteBackend): rename VoteManager

---
+++ Project.VoteBackend/src/Project.ApplicationCore/Aggregates/VotingManagerAggregate/VotingManager.cs
@@ -0,0 +1,10 @@
+﻿using Project.SharedKernel;
+using Project.SharedKernel.Interfaces;
+
+namespace Project.ApplicationCore.Aggregates.VotingManagerAggregate
+{
+  public class VotingManager: BaseEntity<int>, IAggregateRoot
+  {
+    
+  }
+}
