3bbef7e5dab8449a0ab7784d9ee7f1b94e33ddc7
Rostislav Statko <rostislav.statko@gmail.com>
2022-02-12 12:50:09 +0300

    refactor: add more ErrorResponseTypes for oauth server

--- backend/src/Sentaku.AuthServer/AuthServer/AuthServerConstants.cs
+++ backend/src/Sentaku.AuthServer/AuthServer/AuthServerConstants.cs
@@ -51,6 +51,34 @@
       /// exceeds the scope granted by the resource owner.
       /// </summary>
       public const string InvalidScope = "invalid_scope";
+
+      /// <summary>
+      /// The resource owner or authorization server denied the request.
+      /// </summary>
+      public const string AccessDenied = "access_denied";
+
+      /// <summary>
+      /// The authorization server does not support obtaining an authorization code using this method.
+      /// </summary>
+      public const string UnsupportedResponseType = "unsupported_response_type";
+
+      /// <summary>
+      /// The authorization server encountered an unexpected
+      /// condition that prevented it from fulfilling the request.
+      /// (This error code is needed because a 500 Internal Server
+      /// Error HTTP status code cannot be returned to the client
+      /// via an HTTP redirect.)
+      /// </summary>
+      public const string ServerError = "server_error";
+      
+      /// <summary>
+      /// The authorization server is currently unable to handle
+      /// the request due to a temporary overloading or maintenance
+      /// of the server.  (This error code is needed because a 503
+      /// Service Unavailable HTTP status code cannot be returned
+      /// to the client via an HTTP redirect.)
+      /// </summary>
+      public const string TemporarilyUnavailable = "temporarily_unavailable";
     }
     public static class CodeChallengeMethods
     {
