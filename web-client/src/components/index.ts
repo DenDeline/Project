497d8240b419c05654fccd87a61ddcf5c876e7e2
Rostislav Statko <rostislav.statko@gmail.com>
2022-03-19 17:30:51 +0300

    refactor(web-client): add components module

---
+++ web-client/src/components/index.ts
@@ -0,0 +1,9 @@
+export { default as ConfigureUserDialog } from './ConfigureUserDialog'
+
+export { default as Header } from './Header'
+
+export { default as Layout } from './Layout'
+
+export { default as Navbar } from './Navbar'
+
+export { default as SignInUserBadge } from './SignInUserBadge'
