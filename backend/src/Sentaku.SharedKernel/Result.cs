01be809879cbad1d1ea6584365f1f67757a889ea
Rostislav Statko <rostislav.statko@gmail.com>
2022-04-07 23:04:23 +0300

    feat(SharedKernel): implement Result without value

---
+++ backend/src/Sentaku.SharedKernel/Result.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Ardalis.Result;
+
+public class Result: IResult
+{
+  public Result() { }
+
+  private Result(ResultStatus status) => this.Status = status;
+
+  public ResultStatus Status { get; }
+  public IEnumerable<string> Errors { get; private init; }
+  public List<ValidationError> ValidationErrors { get; private init; }
+  
+  public Type? ValueType { get; }
+  
+  public bool IsSuccess => this.Status == ResultStatus.Ok;
+  public string SuccessMessage { get; private init; } = string.Empty;
+
+  public object? GetValue() => null;
+
+  public static Result Success() => new();
+
+  public static Result Success(string successMessage) => new()
+  {
+    SuccessMessage = successMessage
+  };
+
+  public static Result Error(params string[] errorMessages) => new(ResultStatus.Error)
+  {
+    Errors = errorMessages
+  };
+
+  public static Result Invalid(List<ValidationError> validationErrors) => new(ResultStatus.Invalid)
+  {
+    ValidationErrors = validationErrors
+  };
+
+  public static Result NotFound() => new(ResultStatus.NotFound);
+
+  public static Result Forbidden() => new(ResultStatus.Forbidden);
+
+  public static Result Unauthorized() => new(ResultStatus.Unauthorized);
+}
